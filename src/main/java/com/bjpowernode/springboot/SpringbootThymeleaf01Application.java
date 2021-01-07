da3f7003d1248c4a6198c3f6a0240df6ae8afa12
刘亦菲 <1416325479@qq.com>
2021-01-07 19:15:31 +0800

    测试上传

--- src/main/java/com/bjpowernode/springboot/SpringbootThymeleaf01Application.java
+++ src/main/java/com/bjpowernode/springboot/SpringbootThymeleaf01Application.java
@@ -8,7 +8,7 @@ public class SpringbootThymeleaf01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootThymeleaf01Application.class, args);
-		System.out.println("啊啊啊啊啊啊啊");
+
 		
 	}
 
